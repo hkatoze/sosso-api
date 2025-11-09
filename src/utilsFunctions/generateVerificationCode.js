40a94e8e6cc5904ef6c73d3151af182341d8084d
Harouna KINDA <harounakinda.pro@gmail.com>
2025-11-09 09:55:33 +0000

    otp width

--- src/utilsFunctions/generateVerificationCode.js
+++ src/utilsFunctions/generateVerificationCode.js
@@ -1,6 +1,6 @@
 function generateVerificationCode() {
-    const min = 100000; // Valeur minimale (inclus)
-    const max = 999999; // Valeur maximale (inclus)
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  module.exports = { generateVerificationCode };
+  const min = 10000; // Valeur minimale (inclus)
+  const max = 99999; // Valeur maximale (inclus)
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+module.exports = { generateVerificationCode };
